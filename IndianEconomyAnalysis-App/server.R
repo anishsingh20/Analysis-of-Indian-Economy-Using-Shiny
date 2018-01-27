d9c37a0213f574647fe4ae3a995fb427d0dbe0a7
anishsingh@20 <loveeanish@gmail.com>
2018-01-28 00:55:08 +0530

    did some data wrangling and data pre-processing on the dataset

--- IndianEconomyAnalysis-App/server.R
+++ IndianEconomyAnalysis-App/server.R
@@ -1,9 +1,12 @@
 require(shiny)
 require(shinydashboard)
-
 require(dplyr)
 require(tidyr)
 require(highcharter)
+require(readr)
 
+#reading the dataset
+
+indiaEco<-read_csv("IndiaEcoData.csv",col_names=TRUE )
 
 
