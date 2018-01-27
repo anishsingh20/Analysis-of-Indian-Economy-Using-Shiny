63778369513d228c1505b3998390419127816ea1
anishsingh@20 <loveeanish@gmail.com>
2018-01-28 01:31:48 +0530

    reshaping the data for future use

--- IndianEconomyAnalysis-App/server.R
+++ IndianEconomyAnalysis-App/server.R
@@ -8,5 +8,12 @@ require(readr)
 #reading the dataset
 
 indiaEco<-read_csv("IndiaEcoData.csv",col_names=TRUE )
+attach(indiaEco)
 
+#using tidyr to do some data transformations and tidying
 
+#converting data from wide to long format
+#gathering years var into key-value pairs
+indiaEcoNew<-indiaEco %>% gather(key="year",value="EcoValues",4:61)
+
+#years now in a single column
